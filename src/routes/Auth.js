378389bc5b68cb349211366568e8e9ad324b40c8
Hye-Ji Na <nhj7911@gmail.com>
2022-03-15 23:13:56 +0900

    #2.1 Login Form part One

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    //console.log(event.target.name); // onChange 이벤트 발생시 email, password를 콘솔에 출력
+    // event로 부터 target을 받아오고 name과 value를 받아옴
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
