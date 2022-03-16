bedd48e3543d6b109a4c4a2910aa950683f346db
Hye-Ji Na <nhj7911@gmail.com>
2022-03-16 23:47:43 +0900

    #2.3 Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
   const onChange = (event) => {
     //console.log(event.target.name); // onChange 이벤트 발생시 email, password를 콘솔에 출력
     // event로 부터 target을 받아오고 name과 value를 받아옴
@@ -15,8 +22,20 @@ const Auth = () => {
       setPassword(value);
     }
   };
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      let data;
+      const auth = getAuth();
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(auth, email, password);
+      } else {
+        data = await signInWithEmailAndPassword(auth, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
@@ -37,7 +56,7 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
       </form>
       <div>
         <button>Continue with Google</button>
